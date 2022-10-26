ecd59cceb54d7417248ed08df0caad72f2d7e4af
Hugo <hugooloya@gmail.com>
2022-10-26 13:39:56 -0600

    LocalStorage first update
    First update of local storage step

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var timeDisplayEl = $('#currentDay');
 var saveBtn = $('.saveBtn');
 var timers = $('.description');
+var testTesty = $('#user-task');
 
 
-
+// This function displays current time in the header
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 };
 
+
+// This function set specific classes according to the current time
 function setClasses() {
   // var currentHour = moment().format('H');
-  var currentHour = 11;
+  var currentHour = 12;
   for (let i = 0; i < timers.length; i++) {
-    console.log(currentHour, timers[i].dataset.hour);
     if (Number(currentHour) > Number(timers[i].dataset.hour)) {
       timers[i].classList.add('past');
     } else if (Number(currentHour) < Number(timers[i].dataset.hour)) {
@@ -25,14 +27,36 @@ function setClasses() {
 };
 
 
-setClasses();
 
 
-saveBtn.click(function () {
+var masterBtn = $('.table');
+masterBtn.click(function (event) {
+  var clickedItem = event.target.id;
+  if (clickedItem === "nine") {
+    var userTask = $('.description').val();
+    var userDesc = userTask.trim();
+    localStorage.setItem(JSON.stringify(timers[0].dataset.hour), JSON.stringify(userDesc));
+    printTask();
+  }
 });
+
+function printTask() {
+  var userDescription = localStorage.getItem(JSON.stringify(timers[0].dataset.hour));
+  if (userDescription !== null) $('.description').val(userDescription);
+  console.log(userDescription);
+}
+
+
 
 setInterval(displayTime, 1000);
 
-// 1. Loop thru timeblocks
-// 2. Inside that loop compare to current hour
+
+// The init() function fires when the page is loaded 
+// function init() {
+  setClasses();
+// }
+// init();
+
+
+
 // 3. Key has to be unique like dataset
