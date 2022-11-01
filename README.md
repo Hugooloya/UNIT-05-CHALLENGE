d39cde7c4a80943d3edbafcb379626eae7abbe66
Hugo <hugooloya@gmail.com>
2022-10-31 20:41:21 -0600

    Local Storage and README updated

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# UNIT-05-CHALLENGE
+
+# Work Day Scheduler
+
+This is a very useful app to plan your days without losing your task if you close the app.
+
+
+
+
+## Usage/Examples
+
+To use this app, simply type your task in the description box and click the save icon to save it.
+If your task is colored gray, means that the event already pass, if it's colored red, means the event is currently happening and if it's green,
+that means is about to happen.
+
+
+
+
+## Screenshots
+
+![App Screenshot](SS%2001.png)
+
--- script.js
+++ script.js
@@ -1,62 +1,61 @@
-var timeDisplayEl = $('#currentDay');
-var saveBtn = $('.saveBtn');
-var timers = $('.description');
-var testTesty = $('#user-task');
-
+var timeDisplayEl = $("#currentDay");
+var saveBtn = $('button[type="submit"]');
+var timers = $(".description");
+var allInputs = $("input");
+var calendar = {};
 
 // This function displays current time in the header
 function displayTime() {
-  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
-};
-
+}
 
 // This function set specific classes according to the current time
 function setClasses() {
-  // var currentHour = moment().format('H');
+  // var currentHour = moment().format("H");
   var currentHour = 12;
   for (let i = 0; i < timers.length; i++) {
     if (Number(currentHour) > Number(timers[i].dataset.hour)) {
-      timers[i].classList.add('past');
+      timers[i].classList.add("past");
     } else if (Number(currentHour) < Number(timers[i].dataset.hour)) {
-      timers[i].classList.add('future');
+      timers[i].classList.add("future");
     } else {
-      timers[i].classList.add('present');
+      timers[i].classList.add("present");
     }
   }
-};
+}
 
+function saveLastTask(event) {
+  var inputEl = $(`input[name="${event.target.id}"]`);
+  calendar = {
+    ...calendar,
+    [event.target.id]: inputEl.val(),
+  };
+  localStorage.setItem("calendar", JSON.stringify(calendar));
+}
 
-
-
-var masterBtn = $('.table');
-masterBtn.click(function (event) {
-  var clickedItem = event.target.id;
-  if (clickedItem === "nine") {
-    var userTask = $('.description').val();
-    var userDesc = userTask.trim();
-    localStorage.setItem(JSON.stringify(timers[0].dataset.hour), JSON.stringify(userDesc));
-    printTask();
-  }
-});
-
-function printTask() {
-  var userDescription = localStorage.getItem(JSON.stringify(timers[0].dataset.hour));
-  if (userDescription !== null) $('.description').val(userDescription);
-  console.log(userDescription);
+function renderLastTask(lastTask) {
+  $.each(lastTask, function (key, value) {
+    $(`input[name="${key}"]`).val(value);
+  });
 }
 
 
+saveBtn.click(function (event) {
+  event.preventDefault();
+  saveLastTask(event);
+});
 
 setInterval(displayTime, 1000);
 
-
-// The init() function fires when the page is loaded 
-// function init() {
+// The init() function fires when the page is loaded
+function init() {
   setClasses();
-// }
-// init();
+  var lastTask = JSON.parse(localStorage.getItem("calendar"));
+  if (lastTask) {
+    renderLastTask(lastTask);
+    calendar = lastTask;
+  }
+}
 
-
-
-// 3. Key has to be unique like dataset
+init();
